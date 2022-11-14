2a9e7f4985b7490c8de7270c127350c0c5defd1b
pkiop <pkiopb@gmail.com>
2022-11-15 00:46:43 +0900

    Feat: add username

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
-import Logo from './Logo';
-
+import { Logo } from '@/components';
+import { useRecoilState } from 'recoil';
+import { userState } from '@/store';
 const Header = () => {
+  const [user] = useRecoilState(userState);
+
   return (
     <div className='m-4' onClick={() => console.log('outer')}>
       <Link href='/'>
         <Logo />
+        {user.name}
       </Link>
     </div>
   );
