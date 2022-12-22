eed645b6199ed9544e2c790acdbd7bfe3a305321
pkiop <pkiopb@gmail.com>
2022-12-22 23:55:53 +0900

    Fix: e2e setting

--- e2e/tests/portfolio.spec.ts
+++ e2e/tests/portfolio.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect } from "@playwright/test";
 
-test("portfolio should have items", async ({ page }) => {
+test("portfolio should render", async ({ page }) => {
   await page.goto("http://localhost:3000/portfolio/1");
-  await expect(page.locator("button")).toContainText("nexon");
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
+  testDir: "e2e",
   use: {
     headless: false,
     viewport: { width: 1280, height: 720 },
