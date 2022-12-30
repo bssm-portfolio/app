8c8c37408e4fada4e18a3289d259658cb834e3d9
pkiop <pkiopb@gmail.com>
2022-12-31 03:15:35 +0900

    Fix: change fixture to fit swagger

--- fixtures/index.ts
+++ fixtures/index.ts
@@ -1,11 +1,60 @@
+import { Portfolio } from "@/types/portfolio.interface";
+
 const avatarUrl =
   "https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_400x400.jpg";
 
-const portfolioUrl =
-  "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F99ACFA3359A6674308";
+const portfolioUrl = "/assets/images/testPortfolioThumbnail.png";
+
+const portfolio: Portfolio = {
+  portfolioId: 0,
+  writer: {
+    memberId: 0,
+    name: "아이유",
+    profileImageUrl: avatarUrl,
+    email: "iu@koko.com",
+  },
+  portfolioType: "VIDEO",
+  title: "포트폴리오 제목입니다.",
+  description: "string",
+  video: {
+    fileName: "string",
+    filePath: "string",
+    fileUid: "string",
+    fileSize: 0,
+  },
+  portfolioUrl: portfolioUrl,
+  thumbnail: {
+    fileName: "string",
+    filePath: "string",
+    fileUid: "string",
+    fileSize: 0,
+  },
+  scope: "PUBLIC",
+  gitUrl: "string",
+  skillList: ["REACT", "JAVA"],
+  contributorList: [
+    {
+      memberId: 0,
+      name: "string",
+      profileImageUrl: "string",
+      email: "string",
+    },
+  ],
+  bookmarks: 120,
+  views: 0,
+  comments: 12,
+  createdDate: new Date("2022-12-01"),
+};
+
+const portfolioList = Array(10)
+  .fill(0)
+  .map(() => portfolio);
 
 const fixture = {
   avatarUrl,
   portfolioUrl,
+  portfolio,
+  portfolioList,
 };
+
 export default fixture;
