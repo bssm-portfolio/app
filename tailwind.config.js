239b7032c4e10a2d5b285b7561d2fe4d98400b3e
pkiop <pkiopb@gmail.com>
2022-12-31 03:15:04 +0900

    Feat: add custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        70: "70rem",
+      },
+      spacing: {
+        74: "74rem",
+      },
+      padding: {
+        1.875: "1.875rem",
+        0.468: "0.468rem",
+      },
+      colors: {
+        blue: "#3E73FB",
+        somago_yellow: "FAAF3D",
+        black: "#000000",
+        white: "#ffffff",
+      },
+      borderWidth: {
+        DEFAULT: "1px",
+        0.5: "0.5px",
+      },
+      fontFamily: {
+        inter: ["Inter var", "sans-serif"],
+      },
+      fontSize: {
+        sxx: [
+          "0.625rem",
+          {
+            lineHeight: "0.75rem",
+            fontWeight: "400",
+          },
+        ],
+      },
+    },
   },
   plugins: [],
 };
