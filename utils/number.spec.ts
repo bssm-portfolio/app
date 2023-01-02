df8a1334a3a81b791cd94ae3a38e140b0dbc5d1a
J1min <aoiharu41@gmail.com>
2023-01-02 19:53:00 +0900

    Feat: date util 테스트 추가

--- utils/number.spec.ts
+++ utils/number.spec.ts
@@ -1,3 +1,29 @@
-test("1 is 1", () => {
-  expect(1).toBe(1);
+import { getKoreanDate, getTimeAgo } from "./date";
+
+test("koreanDate test - before 2000", () => {
+  expect(getKoreanDate(new Date("December 17, 1995"))).toBe("1995년 12월 17일");
+});
+
+test("koreanDate test - after 2000", () => {
+  expect(getKoreanDate(new Date("December 17, 2005"))).toBe("2005년 12월 17일");
+});
+
+test("timeAgo test - before 2000", () => {
+  expect(getTimeAgo(new Date("December 17, 1995"))).toBe("27년 전");
+});
+
+test("timeAgo test - after 2000", () => {
+  expect(getTimeAgo(new Date("December 17, 2005"))).toBe("17년 전");
+});
+
+test("timeAgo test - today", () => {
+  expect(getTimeAgo(new Date("January 2, 2023"))).toBe("20시간 전");
+});
+
+test("timeAgo test - now", () => {
+  expect(getTimeAgo(new Date())).toBe("방금");
+});
+
+test("timeAgo test - future", () => {
+  expect(getTimeAgo(new Date("December 17, 2025"))).toBe("3년 후");
 });
