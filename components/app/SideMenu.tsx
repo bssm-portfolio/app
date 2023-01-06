45da44bccb0a9817a8254061929231fa23078da2
J1min <aoiharu41@gmail.com>
2023-01-06 18:27:44 +0900

    chore: delete unused annotation

--- components/app/SideMenu.tsx
+++ components/app/SideMenu.tsx
@@ -1,51 +1,10 @@
-// import { urlState } from "@/store";
-// import { useRecoilState } from "recoil";
-// import { Button } from "@/components";
 import SideMenuItem from "../portfolio/SideMenuItem";
 
 export default function SideMenu() {
-  // const [url, setUrl] = useRecoilState(urlState);
   return (
     <div className="ml-0 xl:ml-xlarge ">
       <h2 className="text-base font-bold">추천 프로젝트</h2>
       <SideMenuItem />
-      {/* <Button
-        className="bg-blue-50 p-10"
-        onClick={() => setUrl("https://www.nexon.com/Home/Game")}
-      >
-        nexon
-      </Button>
-      <Button
-        className="bg-blue-50 p-10"
-        onClick={() =>
-          setUrl(
-            "https://section.blog.naver.com/BlogHome.naver?directoryNo=0&currentPage=1&groupId=0",
-          )
-        }
-      >
-        naver blog
-      </Button>
-      <Button
-        className="bg-blue-50 p-10"
-        onClick={() => setUrl("https://comic.naver.com/webtoon/weekday")}
-      >
-        naver webtoon
-      </Button>
-      <div>
-        url 입력해서 되는지 테스트 : 선택된 url : {url}
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            const { testurl } = e.target as unknown as {
-              testurl: { value: string };
-            };
-            if (testurl?.value) setUrl(testurl.value);
-          }}
-        >
-          <input name="testurl" />
-          <input type="submit" value="입력한 url 띄우기" />
-        </form>
-      </div> */}
     </div>
   );
 }
