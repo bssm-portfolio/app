82f8d43185fd3771625a0cef76068d696a955220
pkiop <pkiopb@gmail.com>
2023-01-07 01:33:09 +0900

    Fix: 기존 Button HeaderButton으로 이름변경

---
+++ components/atoms/HeaderButton.tsx
@@ -0,0 +1,47 @@
+import React, { ReactNode } from "react";
+
+type ButtonVarient = "primary" | "secondary";
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  type?: "submit" | "button";
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  children?: ReactNode;
+  className?: string;
+  varient?: ButtonVarient;
+}
+
+const getButtonCss = (varient: ButtonVarient) => {
+  const borderColor = varient === "primary" ? "border-black" : "border-blue";
+  const bgColor = varient === "primary" ? "bg-white" : "bg-blue";
+  const color = varient === "primary" ? "text-black" : "text-white";
+
+  return `
+  px-1.875 
+  py-0.468 
+  border-0.5
+  ${borderColor}
+  ${bgColor}
+  ${color}
+  rounded-full 
+  font-inter
+  font-bold
+  text-sm
+`;
+};
+
+export default function HeaderButton({
+  type = "button",
+  varient = "primary",
+  onClick,
+  children,
+  className = "",
+}: ButtonProps) {
+  return (
+    <button
+      className={`${className} ${getButtonCss(varient)}`}
+      onClick={onClick}
+      type={type === "submit" ? "submit" : "button"}
+    >
+      {children}
+    </button>
+  );
+}
