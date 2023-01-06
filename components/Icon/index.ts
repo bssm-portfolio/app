2e58c7e3ff02017a31b04e0f908b57573c070625
pkiop <pkiopb@gmail.com>
2023-01-07 01:36:43 +0900

    Feat: X 아이콘 추가

--- components/Icon/index.ts
+++ components/Icon/index.ts
@@ -1,3 +1,4 @@
 export { default as CommentIcon } from "./CommentIcon";
 export { default as HeartIcon } from "./HeartIcon";
 export { default as ScopeIcon } from "./ScopeIcon";
+export { default as XIcon } from "./XIcon";
