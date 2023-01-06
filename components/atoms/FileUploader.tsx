515a1daeea61ac615405ae163dd398151608282e
pkiop <pkiopb@gmail.com>
2023-01-07 01:33:38 +0900

    Feat: 파일업로더 버튼 추가

---
+++ components/atoms/FileUploader.tsx
@@ -0,0 +1,51 @@
+import classNames from "classnames";
+import React from "react";
+
+type ButtonVarient = "primary" | "secondary";
+interface FileUploaderProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  className?: string;
+  label?: string;
+  varient?: ButtonVarient;
+  id?: string;
+}
+
+const getFileUploaderCss = (varient: ButtonVarient) => {
+  const bgColor = varient === "primary" ? "bg-blue" : "bg-black";
+  return `
+    px-5
+    py-2.5 
+    cursor-pointer 
+    ${bgColor}
+    text-white
+    rounded 
+    font-inter
+    text-sm
+  `;
+};
+
+export default function FileUploader({
+  varient = "primary",
+  onChange,
+  label,
+  className = "",
+  id = "file-uploader",
+}: FileUploaderProps) {
+  return (
+    <>
+      <label
+        htmlFor={id}
+        className={classNames(className, getFileUploaderCss(varient))}
+      >
+        {label}
+      </label>
+      <input
+        type="file"
+        id={id}
+        className={classNames("hidden")}
+        onChange={onChange}
+      />
+    </>
+  );
+}
