dca0a9ef53fe50cb6de88b1ca8d022324d4b825f
J1min <aoiharu41@gmail.com>
2023-01-06 22:08:58 +0900

    Chore: 시멘틱 태그 추가

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -10,14 +10,14 @@ interface FrameProps {
 
 function Frame({ app, sidebar, detail, comment }: FrameProps) {
   return (
-    <div className="pt-[3.25rem] grid grid-rows-[1fr] xl:grid-cols-[minmax(0,_1fr)_26.25rem] xl:grid-rows-[minmax(1fr,_0)_18.75rem] px-4 xl:px-[6.25rem]">
+    <section className="pt-[3.25rem] grid grid-rows-[1fr] xl:grid-cols-[minmax(0,_1fr)_26.25rem] xl:grid-rows-[minmax(1fr,_0)_18.75rem] px-4 xl:px-[6.25rem]">
       <div>
         <div>{app}</div>
         <div>{detail}</div>
       </div>
       <div className="mt-base xl:mt-0 xl:row-span-2">{sidebar}</div>
       <div>{comment}</div>
-    </div>
+    </section>
   );
 }
 
