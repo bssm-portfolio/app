77e52f4fe1a203ee240d9fbeb954f4708cae783f
J1min <aoiharu41@gmail.com>
2023-01-08 16:13:06 +0900

    Fix: 테스트 순서 변경

--- e2e/tests/index.spec.ts
+++ e2e/tests/index.spec.ts
@@ -44,26 +44,18 @@ test("업로드 form이 정상동작 해야한다.", async ({ page }) => {
 
 test("마지막 페이지 제출 가능", async ({ page }) => {
   await page.goto("http://localhost:3000/");
+  await page.getByRole("button", { name: "upload +" }).click();
   await page.getByRole("button", { name: "다음" }).click();
-  await page.getByRole("heading", { name: "공개범위" }).click();
-  await page.getByText("전체").click();
-  await page.getByText("누구나 볼 수 있습니다.").click();
-  await page
-    .locator('[id="__next"] div')
-    .filter({ hasText: "전체누구나 볼 수 있습니다." })
-    .nth(3)
-    .click();
-  await page
-    .locator('[id="__next"] div')
-    .filter({ hasText: "일부링크가 있으면 누구든 볼 수 있습니다." })
-    .nth(3)
-    .click();
-  await page
-    .locator('[id="__next"] div')
-    .filter({ hasText: "비공개나와 내가 선택한 사람만 볼 수 있습니다." })
-    .nth(3)
-    .click();
+  await page.getByRole("button", { name: "다음" }).click();
   await page.getByRole("radio").nth(1).check();
   await page.getByRole("radio").nth(2).check();
+  await page.getByText("비공개").click();
+  await page.getByText("나와 내가 선택한 사람만 볼 수 있습니다.").click();
+  await page.getByText("링크가 있으면 누구든 볼 수 있습니다.").click();
+  await page.getByText("일부").click();
+  await page.getByText("누구나 볼 수 있습니다.").click();
+  await page.getByText("전체").click();
+  await page.getByRole("heading", { name: "공개범위" }).click();
+  await page.getByRole("heading", { name: "업로드" }).click();
   await page.getByRole("button", { name: "완료" }).click();
 });
