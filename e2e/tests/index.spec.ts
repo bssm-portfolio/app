33797abe6bcf5b0f83931bd83e25d70822587f36
J1min <aoiharu41@gmail.com>
2023-01-08 15:48:07 +0900

    e2e: submit form

--- e2e/tests/index.spec.ts
+++ e2e/tests/index.spec.ts
@@ -1,5 +1,7 @@
 import { test } from "@playwright/test";
 
+test.use({ viewport: { width: 1440, height: 1080 } });
+
 test("Mainpage should have searchbar", async ({ page }) => {
   await page.goto("http://localhost:3000/");
   await page.getByRole("textbox").click();
@@ -38,4 +40,30 @@ test("업로드 form이 정상동작 해야한다.", async ({ page }) => {
     .getByPlaceholder("https://github.com/")
     .fill("깃헙주소 입력가능해야합니다.");
   await page.getByRole("button", { name: "이전" }).click();
+});
+
+test("마지막 페이지 제출 가능", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.getByRole("button", { name: "다음" }).click();
+  await page.getByRole("heading", { name: "공개범위" }).click();
+  await page.getByText("전체").click();
+  await page.getByText("누구나 볼 수 있습니다.").click();
+  await page
+    .locator('[id="__next"] div')
+    .filter({ hasText: "전체누구나 볼 수 있습니다." })
+    .nth(3)
+    .click();
+  await page
+    .locator('[id="__next"] div')
+    .filter({ hasText: "일부링크가 있으면 누구든 볼 수 있습니다." })
+    .nth(3)
+    .click();
+  await page
+    .locator('[id="__next"] div')
+    .filter({ hasText: "비공개나와 내가 선택한 사람만 볼 수 있습니다." })
+    .nth(3)
+    .click();
+  await page.getByRole("radio").nth(1).check();
+  await page.getByRole("radio").nth(2).check();
+  await page.getByRole("button", { name: "완료" }).click();
 });
