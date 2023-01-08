b0364b6bc13fd4dbe1ff60a4fdd92d3be9e2cc35
J1min <aoiharu41@gmail.com>
2023-01-08 15:33:05 +0900

    Fix: closeModal to props

--- components/common/Header/UploadModal/index.tsx
+++ components/common/Header/UploadModal/index.tsx
@@ -1,25 +1,22 @@
-import { modalState } from "@/store";
 import React, { useState } from "react";
-import { useRecoilState } from "recoil";
 import FileUploadView from "./FileUploadView";
 import FormView from "./FormView";
 import Navigator from "./Navigator";
 import SubmitView from "./SubmitView";
 
+interface UploadModalProps {
+  closeModal: () => void;
+}
+
 const MAX_NAVIGATOR_LENGTH = 3;
-export default function UploadModal() {
+export default function UploadModal({ closeModal }: UploadModalProps) {
   const [pageIndex, setPageIndex] = useState(0);
 
   const goNext = () =>
     setPageIndex((idx) => (idx < MAX_NAVIGATOR_LENGTH ? idx + 1 : idx));
   const goPrev = () => setPageIndex((idx) => (idx > 0 ? idx - 1 : idx));
-  const [, setModal] = useRecoilState(modalState);
   const onSubmit = () => {
-    setModal({
-      title: "",
-      content: null,
-      visible: false,
-    });
+    closeModal();
   };
 
   return (
