1cc447adeeed7c95a43856ab5da9ef332fc276d2
J1min <aoiharu41@gmail.com>
2023-01-08 15:06:25 +0900

    Feat: submit시 모달 닫기

--- components/common/Header/UploadModal/index.tsx
+++ components/common/Header/UploadModal/index.tsx
@@ -1,7 +1,10 @@
+import { modalState } from "@/store";
 import React, { useState } from "react";
+import { useRecoilState } from "recoil";
 import FileUploadView from "./FileUploadView";
 import FormView from "./FormView";
 import Navigator from "./Navigator";
+import SubmitView from "./SubmitView";
 
 const MAX_NAVIGATOR_LENGTH = 3;
 export default function UploadModal() {
@@ -10,12 +13,20 @@ export default function UploadModal() {
   const goNext = () =>
     setPageIndex((idx) => (idx < MAX_NAVIGATOR_LENGTH ? idx + 1 : idx));
   const goPrev = () => setPageIndex((idx) => (idx > 0 ? idx - 1 : idx));
-  const onSubmit = () => console.log("submit");
+  const [, setModal] = useRecoilState(modalState);
+  const onSubmit = () => {
+    setModal({
+      title: "",
+      content: null,
+      visible: false,
+    });
+  };
 
   return (
     <>
       {pageIndex === 0 && <FileUploadView />}
       {pageIndex === 1 && <FormView />}
+      {pageIndex === 2 && <SubmitView />}
       <Navigator
         goNext={goNext}
         goPrev={pageIndex > 0 ? goPrev : null}
