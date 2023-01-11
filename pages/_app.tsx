16531427696f4098c4cffb5a6b3bdba535a8c76f
pkiop <pkiopb@gmail.com>
2023-01-11 17:58:16 +0900

    Feat: react query 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,28 @@ import "../styles/tailwind.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import Modal from "@/components/common/Modal";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import config from "@/config";
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
-      <Modal />
+      <QueryClientProvider client={client}>
+        {config.nodeEnv === "development" ? <ReactQueryDevtools /> : null}
+        <Component {...pageProps} />
+        <Modal />
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
