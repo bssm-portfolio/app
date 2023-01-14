61d1437f15299fe6cab2b0f40133913fd6cc3a01
J1min <aoiharu41@gmail.com>
2023-01-14 16:09:41 +0900

    Feat: delete default css height property

--- components/portfolio/SideMenuItem.tsx
+++ components/portfolio/SideMenuItem.tsx
@@ -7,16 +7,16 @@ import Chip from "../atoms/Chip";
 export default function SideMenuItem() {
   const { list } = usePortfolioList();
   return (
-    <div className="flex flex-col items-start xl:w-[26.25rem]">
+    <div className="flex flex-col items-start">
       {list.map((portfolio) => {
         return (
-          <div className="w-full h-full flex m-2" key={portfolio.portfolioId}>
+          <div className="w-full h-full flex my-2" key={portfolio.portfolioId}>
             <Image
-              className="rounded-xsmall"
+              className="rounded-xsmall xl:w-320 xl:h-235 object-cover"
               src={getFileDownloadUrl(portfolio.thumbnail)}
               alt={portfolio.title}
-              width="320"
-              height="180"
+              width="240"
+              height="135"
             />
             <div className="ml-3">
               <h2 className="font-bold text-middle mb-[.5rem]">
@@ -25,14 +25,14 @@ export default function SideMenuItem() {
               <span className="font-bold text-small block mb-xsmall">
                 {portfolio.writer.name}
               </span>
-              <Chip.Group className="mb-xsmall">
+              <Chip.Group>
                 {portfolio.skillList.map((skillData) => {
                   return <Chip.Item key={skillData}>{skillData}</Chip.Item>;
                 })}
               </Chip.Group>
-              <div className="text-xsmall">
+              <span className="text-xsmall">
                 조회수 {portfolio.views}회 · {getTimeAgo(portfolio.createdDate)}
-              </div>
+              </span>
             </div>
           </div>
         );
