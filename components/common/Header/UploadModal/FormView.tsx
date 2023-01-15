0a1fe649593ccb11331103ca0c196329e2e16d9b
J1min <aoiharu41@gmail.com>
2023-01-15 22:40:50 +0900

    Feat: 기술스택 검색 input에 돋보기 아이콘 추가

--- components/common/Header/UploadModal/FormView.tsx
+++ components/common/Header/UploadModal/FormView.tsx
@@ -1,6 +1,7 @@
 import Input from "@/components/atoms/Input";
 import LabelForm from "@/components/atoms/LabelForm";
 import Textarea from "@/components/atoms/Textarea";
+import SearchIcon from "@/components/Icon/SearchIcon";
 
 export default function FormView() {
   return (
@@ -11,8 +12,9 @@ export default function FormView() {
       <LabelForm label="설명" className="mb-6">
         <Textarea className="w-full" placeholder="설명" />
       </LabelForm>
-      <LabelForm label="기술스택(중복선택 가능)" className="mb-6">
-        <Input className="w-full" placeholder="기술스택을 입력하세요" />
+      <LabelForm label="기술스택(중복선택 가능)" className="relative mb-6">
+        <SearchIcon className="absolute top-9 left-3 w-[0.8125rem]" />
+        <Input className="w-full pl-8" placeholder="기술스택을 입력하세요" />
       </LabelForm>
       <LabelForm label="참여자 아이디" className="mb-6">
         <Input className="w-full" placeholder="#해시태그" />
