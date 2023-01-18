e655b368c7c1989ed6531952104d62b23f358ca4
J1min <aoiharu41@gmail.com>
2023-01-19 03:01:17 +0900

    Feat: add router at uploadModal submit

--- components/common/Header/UploadModal/index.tsx
+++ components/common/Header/UploadModal/index.tsx
@@ -1,3 +1,4 @@
+import { NextRouter, useRouter } from "next/router";
 import { useState } from "react";
 import FileUploadView from "./FileUploadView";
 import FormView from "./FormView";
@@ -9,13 +10,18 @@ interface UploadModalProps {
 }
 
 const MAX_NAVIGATOR_LENGTH = 3;
+const onSubmit = (closeModal: () => void, router: NextRouter) => {
+  closeModal();
+  router.push("/contents");
+};
+
 export default function UploadModal({ closeModal }: UploadModalProps) {
+  const router = useRouter();
   const [pageIndex, setPageIndex] = useState(0);
 
   const goNext = () =>
     setPageIndex((idx) => (idx < MAX_NAVIGATOR_LENGTH ? idx + 1 : idx));
   const goPrev = () => setPageIndex((idx) => (idx > 0 ? idx - 1 : idx));
-  const onSubmit = () => closeModal();
 
   return (
     <div className="h-40">
@@ -25,7 +31,11 @@ export default function UploadModal({ closeModal }: UploadModalProps) {
       <Navigator
         goNext={goNext}
         goPrev={pageIndex > 0 ? goPrev : null}
-        onSubmit={pageIndex === MAX_NAVIGATOR_LENGTH - 1 ? onSubmit : null}
+        onSubmit={
+          pageIndex === MAX_NAVIGATOR_LENGTH - 1
+            ? () => onSubmit(closeModal, router)
+            : null
+        }
       />
     </div>
   );
