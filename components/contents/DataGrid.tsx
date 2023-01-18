28086bb0d3d51ff8410527e5d0ccc82b40745206
J1min <aoiharu41@gmail.com>
2023-01-18 23:38:28 +0900

    Feat: add datagrid data

--- components/contents/DataGrid.tsx
+++ components/contents/DataGrid.tsx
@@ -1,5 +1,61 @@
-import React from "react";
+import { Portfolio } from "@/types/portfolio.interface";
+import Image from "next/image";
+import React, { useState } from "react";
+import CheckBox from "../atoms/CheckBox";
 
-export default function DataGrid() {
-  return <div>DataGrid</div>;
+interface DataGridProps {
+  portfolioList: Portfolio[];
+}
+
+interface Selected {
+  portfolioId: string;
+  isSelected: boolean;
+}
+
+export default function DataGrid({ portfolioList }: DataGridProps) {
+  const [selected, setSelected] = useState<Selected[]>([]);
+
+  const selectPortfolio = (portfolioId: string) => {
+    setSelected((prev) => [...prev, { portfolioId, isSelected: false }]);
+  };
+
+  return (
+    <table className="w-full">
+      <thead className="border-y border-y-primary-dark_gray">
+        <tr>
+          <th className="flex items-center py-6 pl-14 text-start">
+            <CheckBox id="select-all" className="mr-3" />
+            <label htmlFor="select-all">전체선택</label>
+          </th>
+          <th className="py-6 w-[7.75rem]">조회수</th>
+          <th className="py-6 w-[7.75rem]">댓글</th>
+          <th className="py-6 w-[7.75rem]">좋아요</th>
+        </tr>
+      </thead>
+      <tbody className="text-center">
+        {portfolioList.map((portfolio) => (
+          <tr key={portfolio.portfolioId}>
+            <td className="flex items-center py-4 pl-14 text-start cursor-pointer">
+              <CheckBox
+                value={portfolio.portfolioId}
+                onChange={(event) => selectPortfolio(event.target.value)}
+              />
+              <div className="relative w-[7.5rem] h-[4.2rem] mx-3">
+                <Image
+                  className="object-cover rounded"
+                  src="https://velog.velcdn.com/images/j1min/post/70786da5-54ae-44cc-a6c7-9e9c60751db0/image.png"
+                  alt="펭수"
+                  fill
+                />
+              </div>
+              <h2 className="font-bold">{portfolio.title}</h2>
+            </td>
+            <td className="py-4 w-[7.75rem]">{portfolio.views}</td>
+            <td className="py-4 w-[7.75rem]">{portfolio.comments}</td>
+            <td className="py-4 w-[7.75rem]">{portfolio.bookmarks}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
