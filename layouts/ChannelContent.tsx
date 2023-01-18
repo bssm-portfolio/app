3b1ad62da95f35b88b8a6d211f8f85daa59dbae8
J1min <aoiharu41@gmail.com>
2023-01-18 21:40:22 +0900

    Feat: add chennel content layout

---
+++ layouts/ChannelContent.tsx
@@ -0,0 +1,15 @@
+import { Header } from "@/components";
+import React from "react";
+
+function Frame() {
+  return <div>ㅇㅇ</div>;
+}
+
+export default function ChannelContentLayout() {
+  return (
+    <>
+      <Header />
+      <Frame />
+    </>
+  );
+}
