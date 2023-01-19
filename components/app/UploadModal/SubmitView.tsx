b473943df8514585b602fd27a6818c45637b1771
pkiop <pkiopb@gmail.com>
2023-01-20 01:08:04 +0900

    Feat: add submitView form

--- components/app/UploadModal/SubmitView.tsx
+++ components/app/UploadModal/SubmitView.tsx
@@ -1,30 +1,38 @@
 import Radio from "@/components/atoms/Radio";
+import { PortfolioForm } from "@/types/portfolio.interface";
+import { UseFormRegister } from "react-hook-form";
 
-export default function SubmitView() {
+export default function SubmitView({
+  register,
+  className,
+}: {
+  register: UseFormRegister<PortfolioForm>;
+  className?: string;
+}) {
   return (
-    <>
+    <div className={className}>
       <h2 className="text-base mb-large">공개범위</h2>
-      <form>
-        <Radio
-          id="entire"
-          label="전체"
-          name="open-range"
-          description="누구나 볼 수 있습니다."
-          checked
-        />
-        <Radio
-          id="part"
-          label="일부"
-          name="open-range"
-          description="링크가 있으면 누구든 볼 수 있습니다."
-        />
-        <Radio
-          id="private"
-          label="비공개"
-          name="open-range"
-          description="나와 내가 선택한 사람만 볼 수 있습니다."
-        />
-      </form>
-    </>
+      <Radio
+        registerReturn={register("portfolioScope", { required: true })}
+        id="entire"
+        label="전체"
+        value="PUBLIC"
+        description="누구나 볼 수 있습니다."
+      />
+      <Radio
+        registerReturn={register("portfolioScope", { required: true })}
+        id="part"
+        label="일부"
+        value="PROTECTED"
+        description="링크가 있으면 누구든 볼 수 있습니다."
+      />
+      <Radio
+        registerReturn={register("portfolioScope", { required: true })}
+        id="private"
+        label="비공개"
+        value="PRIVATE"
+        description="나와 내가 선택한 사람만 볼 수 있습니다."
+      />
+    </div>
   );
 }
