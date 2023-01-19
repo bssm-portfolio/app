b533174c085e8fcb0af716fc45120d11e4062dda
J1min <aoiharu41@gmail.com>
2023-01-20 01:54:05 +0900

    Chore: change util function name

--- utils/data.ts
+++ utils/data.ts
@@ -1,6 +1,6 @@
 import { Portfolio } from "@/types/portfolio.interface";
 
 // eslint-disable-next-line import/prefer-default-export
-export const getParsedDataGridItems = (portfolioList: Portfolio[]) => {
+export const getParsedDataGridList = (portfolioList: Portfolio[]) => {
   return JSON.parse(JSON.stringify(portfolioList));
 };
