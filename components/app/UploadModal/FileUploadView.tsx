d9e496a4500b6768aae8fe985aaf6e6c33838c14
pkiop <pkiopb@gmail.com>
2023-01-20 01:28:40 +0900

    Fix: typo

--- components/app/UploadModal/FileUploadView.tsx
+++ components/app/UploadModal/FileUploadView.tsx
@@ -15,7 +15,7 @@ export default function FileUploadView({
   return (
     <div className={classNames("flex flex-col gap-8", className)} {...props}>
       <div>
-        <h2 className="mb-2">웹 주소</h2>
+        <h2 className="mb-2">동영상</h2>
         <div className="w-full border h-60 mb-2.5 flex flex-col justify-center items-center border-primary-border_gray gap-2.5 rounded-lg">
           <FileUploader label="동영상 업로드" />
           <p>동영상 파일을 드래그 앤 드롭하여 업로드</p>
