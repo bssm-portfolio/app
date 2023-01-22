e38544a9257e1f7fa8d02d0d9ddead274038e036
J1min <aoiharu41@gmail.com>
2023-01-22 22:18:57 +0900

    Feat: add login button

---
+++ components/atoms/LoginButton.tsx
@@ -0,0 +1,24 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+
+interface LoginButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  type?: "submit" | "button";
+  children: ReactNode;
+}
+
+export default function LoginButton({
+  type = "button",
+  children,
+}: LoginButtonProps) {
+  const getButtonCss = () => {
+    return `border border-primary-dark_gray w-full rounded-full py-5 text-[1.125rem] font-bold flex justify-center items-center`;
+  };
+
+  return (
+    <button
+      type={type === "submit" ? "submit" : "button"}
+      className={getButtonCss()}
+    >
+      {children}
+    </button>
+  );
+}
