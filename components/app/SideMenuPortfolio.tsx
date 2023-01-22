b269b2f496e0cf67635d7c94fbdae42f2e42644b
J1min <aoiharu41@gmail.com>
2023-01-23 00:50:52 +0900

    Fix: change image size

--- components/app/SideMenuPortfolio.tsx
+++ components/app/SideMenuPortfolio.tsx
@@ -16,19 +16,21 @@ function SideMenuPortfolio({ portfolio, onClick }: SideMenuPortfolioProps) {
       key={portfolio.portfolioId}
       onClick={onClick}
     >
-      <Image
-        className="rounded-xsmall"
-        src={getFileDownloadUrl(portfolio.thumbnail)}
-        alt={portfolio.title}
-        width="320"
-        height="180"
-      />
+      <div className="relative w-[320px] h-[180px] xl:w-[240px] xl:h-[135px]">
+        <Image
+          className="rounded-[0.625rem]"
+          src={getFileDownloadUrl(portfolio.thumbnail)}
+          alt={portfolio.title}
+          fill
+        />
+      </div>
+
       <div className="ml-3">
         <h2 className="font-bold text-middle mb-[.5rem]">{portfolio.title}</h2>
         <span className="font-bold text-small block mb-xsmall">
           {portfolio.writer.name}
         </span>
-        <Chip.Group className="mb-xsmall">
+        <Chip.Group className="mb-xsmall" type="portfolio">
           {portfolio.skillList.map((skillData) => {
             return <Chip.Item>{skillData}</Chip.Item>;
           })}
