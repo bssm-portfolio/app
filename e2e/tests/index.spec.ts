f9b31fe9e4ba85502efa78a9b099b213d3161a86
J1min <aoiharu41@gmail.com>
2023-01-28 17:18:24 +0900

    Fix: 테스트 재시도

--- e2e/tests/index.spec.ts
+++ e2e/tests/index.spec.ts
@@ -34,20 +34,19 @@ test("업로드 form이 정상동작 해야한다.", async ({ page }) => {
   await page.getByRole("button", { name: "이전" }).click();
 });
 
-test("마지막 페이지 제출 가능", async ({ page }) => {
+test("마지막 페이지 테스트", async ({ page }) => {
   await page.goto("http://localhost:3000/");
   await page.getByRole("button", { name: "upload +" }).click();
-  await page.getByRole("button", { name: "다음" }).click();
-  await page.getByRole("button", { name: "다음" }).click();
-  await page.getByRole("radio").nth(1).check();
-  await page.getByRole("radio").nth(2).check();
+  await page.getByRole("button", { name: "다음" }).dblclick();
+  await page.getByRole("heading", { name: "공개범위" }).click();
+  await page.locator("label").filter({ hasText: "전체" }).click();
+  await page.getByLabel("전체").check();
+  await page.getByText("누구나 볼 수 있습니다.").click();
+  await page.getByLabel("일부").check();
+  await page.getByText("일부").click();
+  await page.getByText("링크가 있으면 누구든 볼 수 있습니다.").click();
+  await page.getByLabel("비공개").check();
   await page.getByText("비공개").click();
   await page.getByText("나와 내가 선택한 사람만 볼 수 있습니다.").click();
-  await page.getByText("링크가 있으면 누구든 볼 수 있습니다.").click();
-  await page.getByText("일부").click();
-  await page.getByText("누구나 볼 수 있습니다.").click();
-  await page.getByText("전체").click();
-  await page.getByRole("heading", { name: "공개범위" }).click();
-  await page.getByRole("heading", { name: "업로드" }).click();
   await page.getByRole("button", { name: "완료" }).click();
 });
