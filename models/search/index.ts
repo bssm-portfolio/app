e898deb9a3d904b5d979b3a2f46b371e4330e8de
J1min <aoiharu41@gmail.com>
2023-01-30 19:16:08 +0900

    Fix: 빈 검색어일시 검색 안되도록 변경

--- models/search/index.ts
+++ models/search/index.ts
@@ -3,8 +3,13 @@ import { PortfolioList } from "@/types/portfolio.interface";
 import { useQuery } from "@tanstack/react-query";
 
 const useSearch = (keyword: string) => {
-  const { data } = useQuery<PortfolioList>(["search", keyword], () =>
-    httpClient.portfolio.search({ params: { keyword } }).then((d) => d.data),
+  const { data } = useQuery<PortfolioList>(
+    ["search", keyword],
+    () =>
+      httpClient.portfolio.search({ params: { keyword } }).then((d) => d.data),
+    {
+      enabled: keyword !== "",
+    },
   );
   return data || { pagination: null, list: [] };
 };
