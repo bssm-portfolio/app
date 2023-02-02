a88c884e174443ebf9be86634a43060fab372c5a
J1min <aoiharu41@gmail.com>
2023-02-02 13:41:58 +0900

    Feat: bsm oauth 페이지 추가

---
+++ models/oauth/index.ts
@@ -0,0 +1,28 @@
+import httpClient from "@/apis";
+import { Token } from "@/types/member.interface";
+import { useQuery } from "@tanstack/react-query";
+
+const getOauth = (
+  platform: string,
+  authCode: string,
+): (() => Promise<Token>) => {
+  if (platform === "google")
+    return () => httpClient.oauth.google({ authCode }).then((d) => d.data);
+  if (platform === "kakao")
+    return () => httpClient.oauth.kakao({ authCode }).then((d) => d.data);
+  return () => httpClient.oauth.bsm({ authCode }).then((d) => d.data);
+};
+
+const useOauth = (platform: string, authCode: string) => {
+  const { data } = useQuery<Token>(
+    [`${platform} oauth`, authCode],
+    getOauth(platform, authCode),
+    {
+      enabled: !!authCode,
+    },
+  );
+  return data || { token: "", validity: "" };
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { useOauth };
---
+++ pages/oauth/callback/bsm.tsx
@@ -0,0 +1,22 @@
+import { AUTH_TOKEN } from "@/config/const";
+import { useOauth } from "@/models/oauth";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function Bsm() {
+  const router = useRouter();
+  const [authCode, setAuthCode] = useState("");
+  const { token } = useOauth("bsm", authCode);
+
+  useEffect(() => {
+    setAuthCode(router.query.code as string);
+  }, [router.query]);
+
+  useEffect(() => {
+    if (token !== "") {
+      localStorage.setItem(AUTH_TOKEN, token);
+    }
+  }, [token]);
+
+  return <div>{token}</div>;
+}
