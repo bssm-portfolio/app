c92662737d254464103240a10b48eb479296be13
pkiop <pkiopb@gmail.com>
2023-02-08 09:18:51 +0900

    Feat: add storage

---
+++ models/storage/index.ts
@@ -0,0 +1,13 @@
+type LocalStorageKey = "ACCESS_TOKEN" | "REFRESH_TOKEN";
+export class Storage {
+  static getItem(key: LocalStorageKey) {
+    return typeof window !== "undefined" ? localStorage.getItem(key) : null;
+  }
+
+  static setItem(key: LocalStorageKey, value: string) {
+    if (typeof window !== "undefined") return;
+    localStorage.setItem(key, value);
+  }
+}
+
+export default {};
