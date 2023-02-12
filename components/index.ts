99d545f27ff589af4916b49cb05ab4f3d560b9aa
J1min <aoiharu41@gmail.com>
2023-02-12 15:04:03 +0900

    Fix: comment를 commentList로 이름 변경

--- components/index.ts
+++ components/index.ts
@@ -9,7 +9,7 @@ export { default as Portfolio } from "./common/Portfolio";
 export { default as AppDetail } from "./app/Detail";
 export { default as PortfolioPlayer } from "./app/PortfolioPlayer";
 export { default as AppSideMenu } from "./app/SideMenu";
-export { default as AppComment } from "./app/Comment";
+export { default as AppComment } from "./app/CommentList";
 export { default as MyPageProfile } from "./app/MyPageProfile";
 export { default as MyPagePortfolioList } from "./app/MyPagePortfolioList";
 export { default as MainPortfolioList } from "./main/MainPortfolioList";
