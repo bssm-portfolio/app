d56301eeacf0a7d3f443899bd4dff9c02f19d849
J1min <aoiharu41@gmail.com>
2023-02-12 15:04:15 +0900

    Feat: comment type 재지정

--- models/portfolio/index.ts
+++ models/portfolio/index.ts
@@ -1,6 +1,6 @@
 import httpClient from "@/apis";
 import fixture from "@/fixtures";
-import { PortfolioList } from "@/types/portfolio.interface";
+import { Comment, PortfolioList } from "@/types/portfolio.interface";
 import { useQuery } from "@tanstack/react-query";
 
 const usePortfolioList = () => {
@@ -13,19 +13,6 @@ const usePortfolioList = () => {
 const usePortfolio = () => {
   return { data: fixture.portfolio };
 };
-
-interface Comment {
-  writer: {
-    memberId: number;
-    name: string;
-    profileImageUrl: string;
-    email: string;
-  };
-  commentId: number;
-  content: string;
-  createdDate: Date;
-  editable: boolean;
-}
 
 interface CommentList {
   list: Comment[];
