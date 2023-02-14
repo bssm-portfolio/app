95a9bd69849bfe40398f4e087a0c2ab5037db2fb
J1min <aoiharu41@gmail.com>
2023-02-14 17:19:15 +0900

    Feat: add video file uid

--- components/app/UploadModal/FileUploadView.tsx
+++ components/app/UploadModal/FileUploadView.tsx
@@ -3,27 +3,32 @@ import FileUploader from "@/components/atoms/FileUploader";
 import Input from "@/components/atoms/Input";
 import { PortfolioForm } from "@/types/portfolio.interface";
 import classNames from "classnames";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
 import { UseFormRegister } from "react-hook-form";
 
 export default function FileUploadView({
   register,
   className,
+  setVideoFileUid,
+  setThumbnailFileUid,
   ...props
 }: {
   register: UseFormRegister<PortfolioForm>;
   className?: string;
+  setVideoFileUid: Dispatch<SetStateAction<string>>;
+  setThumbnailFileUid: Dispatch<SetStateAction<string>>;
 }) {
   const [fileUploadStatus, setFileUploadStatus] = useState<string>("");
 
-  const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleVideoFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       const formData = new FormData();
       const flieName = e.target.files[0].name;
       formData.append("file", e.target.files[0]);
       setFileUploadStatus(`${flieName} 업로드 중..`);
 
-      httpClient.file.upload(formData).then(() => {
+      httpClient.file.upload(formData).then((r) => {
+        setVideoFileUid(r.data.fileUid);
         setFileUploadStatus(`${flieName} 업로드 완료`);
       });
     }
@@ -38,7 +43,10 @@ export default function FileUploadView({
             <p>{fileUploadStatus}</p>
           ) : (
             <>
-              <FileUploader label="동영상 업로드" onChange={handleFileUpload} />
+              <FileUploader
+                label="동영상 업로드"
+                onChange={handleVideoFileUpload}
+              />
               <p>동영상 파일을 드래그 앤 드롭하여 업로드</p>
             </>
           )}
