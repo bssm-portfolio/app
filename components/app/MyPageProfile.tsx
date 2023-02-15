5b08d5fa23599b2e882dc8e0f0c6d38c74a8096a
J1min <aoiharu41@gmail.com>
2023-02-15 16:37:43 +0900

    Feat: add myProfile

--- components/app/MyPageProfile.tsx
+++ components/app/MyPageProfile.tsx
@@ -4,20 +4,41 @@ import Avatar from "../common/Avatar";
 
 export default function MyPageProfile({ userInfo }: { userInfo: Member }) {
   return (
-    <div className="w-[282px] h-[540px] rounded-lg bg-primary-light_gray px-[52px] py-[30px]">
-      <div>
-        <Avatar imageUrl={userInfo.profileImageUrl} width={128} height={128} />
-        <h2 className="font-bold text-2xl">{userInfo.name}</h2>
-        <h4>{userInfo.job}</h4>
+    <>
+      <div className="flex flex-col w-[17.625rem] h-[33.75rem] rounded-lg bg-primary-light_gray px-[3.25rem] py-[1.875rem]">
+        <div className="flex flex-col items-center">
+          <Avatar
+            imageUrl={userInfo.profileImageUrl}
+            width={128}
+            height={128}
+          />
+          <h2 className="font-bold text-2xl mt-5">{userInfo.name}</h2>
+          <h4 className="font-bold mt-1 text-xs">
+            {userInfo.job ?? "앱개발자"}
+          </h4>
+        </div>
+        <div className="mt-5">
+          <h4 className="font-bold text-xs mb-2">프로젝트 수 {0}</h4>
+          <h4 className="font-bold text-xs mb-2">팔로워 수 {0}</h4>
+          <h4 className="font-bold text-xs">팔로잉 수 {0}</h4>
+          <h4 className="mt-5 text-xsmall">
+            {userInfo.description ?? "안녕하세요 반갑습니다"}
+          </h4>
+          <h4 className="text-2xsmall">{userInfo.email}</h4>
+        </div>
+        <div className="flex justify-center mt-auto">
+          <Button
+            varient="secondary"
+            className="border border-black !rounded-full bg-white py-2 px-8"
+          >
+            수정하기
+          </Button>
+        </div>
       </div>
-      <div>
-        <h4>{userInfo.memberRoleType}</h4>
-        <h4>{userInfo.description}</h4>
-        <h4>{userInfo.email}</h4>
+      <div className="flex flex-col gap-4 mt-8">
+        <Button>채널 콘텐츠 관리</Button>
+        <Button>내 정보 관리</Button>
       </div>
-      <div>
-        <Button varient="secondary">수정하기</Button>
-      </div>
-    </div>
+    </>
   );
 }
