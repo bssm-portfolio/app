1da45f6c60297be924962ac1dbba1f64ce8fd6e9
J1min <aoiharu41@gmail.com>
2023-02-17 22:48:07 +0900

    Feat: add toast types

---
+++ types/toast.interface.ts
@@ -0,0 +1,15 @@
+export type PositionType =
+  | "TOP_LEFT"
+  | "TOP_CENTER"
+  | "TOP_RIGHT"
+  | "BOTTOM_LEFT"
+  | "BOTTOM_CENTER"
+  | "BOTTOM_RIGHT";
+export type ToastType = "success" | "danger" | "normal";
+
+export interface Toast {
+  position?: PositionType;
+  content: string;
+  type?: ToastType;
+  time?: number;
+}
