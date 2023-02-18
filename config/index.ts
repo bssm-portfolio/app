f01cc4f821bb4d4fefc860b259a98b697b213127
J1min <aoiharu41@gmail.com>
2023-02-18 20:41:29 +0900

    Feat: delete env client url

--- config/index.ts
+++ config/index.ts
@@ -1,18 +1,15 @@
 interface Config {
   baseURL: string;
-  clientUrl: string;
   nodeEnv: "development" | "production" | "test";
 }
 
 const createConfig: () => Config = () => {
   if (!process.env.NEXT_PUBLIC_SERVER_URL) throw new Error("no api server url");
-  if (!process.env.NEXT_PUBLIC_CLIENT_URL) throw new Error("no clinet url ");
   if (!process.env.NODE_ENV) throw new Error("no node env ");
 
   return {
     baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
     nodeEnv: process.env.NODE_ENV,
-    clientUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
   };
 };
 
