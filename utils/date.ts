df007a42a1942158a1bdc3a42e2a5f7a7f012579
J1min <aoiharu41@gmail.com>
2023-02-18 21:49:37 +0900

    Fix: 00초 일 때 date로 형변환 안되는 버그 수정

--- utils/date.ts
+++ utils/date.ts
@@ -16,12 +16,12 @@ export const getKoreanDate = (
 };
 
 export const getTimeAgo = (date: Date) => {
-  return timeAgo.format(date);
+  return timeAgo.format(new Date(date));
 };
 
 export const isISODateString = (value: string) => {
   const isISODateStringRegExp =
-    /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$/;
+    /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):?([0-5][0-9])?(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$/;
   return isISODateStringRegExp.test(value);
 };
 
