b81d6e159d69e70aae524e8286c6154c85acb541
J1min <aoiharu41@gmail.com>
2023-02-18 12:25:11 +0900

    Fix: 애니메이션 변경, 위치 고정

--- components/common/Toast/View.tsx
+++ components/common/Toast/View.tsx
@@ -12,25 +12,25 @@ export default function ToastView({ toastList }: ToastViewProps) {
     normal: "bg-[#3E73FB]",
   };
   const toastAnimationList = {
-    TOP_LEFT: "animate-[toast-in-left_3s_ease-in-out]",
-    TOP_RIGHT: "animate-[toast-in-right_3s_ease-in-out]",
-    TOP_CENTER: "animate-[toast-in-bottom_3s_ease-in-out]",
-    BOTTOM_LEFT: "animate-[toast-in-left_3s_ease-in-out]",
-    BOTTOM_RIGHT: "animate-[toast-in-right_3s_ease-in-out]",
-    BOTTOM_CENTER: "animate-[toast-in-top_3s_ease-in-out]",
+    TOP_LEFT: "animate-[toast-in-left_3s_ease]",
+    TOP_RIGHT: "animate-[toast-in-right_3s_ease]",
+    TOP_CENTER: "animate-[toast-in-bottom_3s_ease]",
+    BOTTOM_LEFT: "animate-[toast-in-left_3s_ease]",
+    BOTTOM_RIGHT: "animate-[toast-in-right_3s_ease]",
+    BOTTOM_CENTER: "animate-[toast-in-top_3s_ease]",
   };
 
   return (
-    <div className="fixed bottom-6 right-6">
+    <div className="fixed bottom-6 right-3">
       {toastList.map((toast) => {
         return (
           <div
-            key={toast.id}
             className={classNames(
-              "p-4 text-white rounded-xl",
+              "p-4 text-white rounded-xl mb-1",
               backgroundColorList[toast.type],
               toastAnimationList[toast.position],
             )}
+            key={toast.id}
           >
             {toast.content}
           </div>
