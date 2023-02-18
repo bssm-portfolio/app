75001cf3bb433155d6630e9c61689b00cfc45fb5
J1min <aoiharu41@gmail.com>
2023-02-18 22:54:59 +0900

    Feat: 둥글둥글한 + 아이콘 추가

---
+++ components/Icon/RoundPlusIcon.tsx
@@ -0,0 +1,23 @@
+export default function RoundPlusIcon() {
+  return (
+    <svg
+      width="26"
+      height="26"
+      viewBox="0 0 26 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13.0002 8.19995V13M13.0002 13V17.8M13.0002 13H17.8002M13.0002 13H8.2002"
+        stroke="#3E73FB"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+      <path
+        d="M13 25C19.6274 25 25 19.6274 25 13C25 6.37258 19.6274 1 13 1C6.37258 1 1 6.37258 1 13C1 19.6274 6.37258 25 13 25Z"
+        stroke="#3E73FB"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
