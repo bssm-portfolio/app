df9833fb9a67c19b38d693d8509c4ecc4b13b62c
J1min <aoiharu41@gmail.com>
2023-02-21 17:54:44 +0900

    Feat: main title, filter 추가

---
+++ components/main/MainTitle.tsx
@@ -0,0 +1,10 @@
+export default function MainTitle() {
+  return (
+    <div>
+      <h1>BUSAN</h1>
+      <h1>SOFTWARE MEISTER</h1>
+      <h1>HIGH SCHOOL</h1>
+      <hr className="text-white" />
+    </div>
+  );
+}
