d0ce27058ed1d690dd5ab6893041e249771a9002
J1min <aoiharu41@gmail.com>
2023-02-21 22:43:40 +0900

    Chore: handleFileUpload util로 이동

--- utils/file.ts
+++ utils/file.ts
@@ -1,8 +1,24 @@
+import httpClient from "@/apis";
 import config from "@/config";
 import { S3File } from "@/types/file.interface";
+import { ChangeEvent, Dispatch, SetStateAction } from "react";
 
 export const getFileDownloadUrl = (file: S3File) => {
   return `${config.baseURL}/api/file/download/${file.fileUid}`;
+};
+
+export const handleFileUpload = (
+  event: ChangeEvent<HTMLInputElement>,
+  setState: Dispatch<SetStateAction<string>>,
+) => {
+  if (event.target.files) {
+    const formData = new FormData();
+    formData.append("file", event.target.files[0]);
+
+    httpClient.file.upload(formData).then((r) => {
+      setState(r.data.fileUid);
+    });
+  }
 };
 
 export default {};
