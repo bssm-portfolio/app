f93a963a9c6817c83c232455b5aa16b84a732945
J1min <aoiharu41@gmail.com>
2023-02-23 23:19:43 +0900

    Chore: 안쓰는 import 삭제

--- components/app/UploadModal/index.tsx
+++ components/app/UploadModal/index.tsx
@@ -1,5 +1,5 @@
 import httpClient from "@/apis";
-import { PORTFOLIO_LIST_KEY } from "@/models/key";
+import { PORTFOLIO_LIST_BY_ID_KEY, PORTFOLIO_LIST_KEY } from "@/models/key";
 import { PortfolioForm, PortfolioType } from "@/types/portfolio.interface";
 import { Skill } from "@/types/skill.interface";
 import { getFormData } from "@/utils/file";
@@ -62,7 +62,9 @@ export default function UploadModal({ closeModal }: UploadModalProps) {
         })
         .then(() => {
           closeModal();
-          queryClient.invalidateQueries({ queryKey: [PORTFOLIO_LIST_KEY] });
+          queryClient.invalidateQueries({
+            queryKey: [PORTFOLIO_LIST_KEY],
+          });
         });
     }
   };
