2788989fb8609c4341a643a27f4b0b5f6401333d
J1min <aoiharu41@gmail.com>
2023-02-24 16:57:22 +0900

    Feat: useMember hooks 추가

---
+++ models/member/index.ts
@@ -0,0 +1,21 @@
+import httpClient from "@/apis";
+import fixture from "@/fixtures";
+import { Member } from "@/types/member.interface";
+import { useQuery } from "@tanstack/react-query";
+import KEY from "../key";
+
+const useMember = (memberId: number) => {
+  const { data } = useQuery<Member>(
+    [KEY.MEMBER],
+    () =>
+      httpClient.member
+        .getById({ params: { id: memberId } })
+        .then((r) => r.data),
+    {
+      enabled: !!memberId,
+    },
+  );
+  return data || fixture.user;
+};
+
+export default useMember;
