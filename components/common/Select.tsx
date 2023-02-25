f4b4152a1f5e71102d8a7fab56b784572d72a237
pkiop <pkiopb@gmail.com>
2023-02-25 16:52:12 +0900

    Feat: options props로 받도록 수정, react-select 추가

--- components/common/Select.tsx
+++ components/common/Select.tsx
@@ -1,29 +1,43 @@
-import { ReactNode, SelectHTMLAttributes } from "react";
+import { SelectHTMLAttributes } from "react";
+import ReactSelect from "react-select";
 import DownIcon from "../Icon/DownIcon";
 
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   className?: string;
-  children: ReactNode;
+  options?: {
+    value: string | number | readonly string[] | undefined;
+    label: string;
+  }[];
+  isNativeSelect?: boolean;
 }
 
 export default function Select({
-  children,
   onChange,
   name,
   className = "",
+  options = [],
+  isNativeSelect = false,
+  ...props
 }: SelectProps) {
-  return (
-    <div className={className}>
-      <div className="relative">
-        <select
-          name={name}
-          onChange={onChange}
-          className="appearance-none pr-8 text-center bg-inherit focus:outline-none"
-        >
-          {children}
-        </select>
-        <DownIcon className="absolute top-2 right-3 -z-10" />
+  if (isNativeSelect) {
+    return (
+      <div className={className}>
+        <div className="relative">
+          <select
+            name={name}
+            onChange={onChange}
+            className="appearance-none pr-8 text-center bg-inherit focus:outline-none"
+          >
+            {options.map(({ label, value }) => (
+              <option key={label + value} value={value}>
+                {label}
+              </option>
+            ))}
+          </select>
+          <DownIcon className="absolute top-2 right-3 -z-10" />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return <ReactSelect placeholder={props.placeholder} options={options} />;
 }
