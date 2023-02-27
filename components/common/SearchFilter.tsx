089fccf9254f49d7f1a677cc19cf496ebaf0b92b
J1min <aoiharu41@gmail.com>
2023-02-27 17:32:15 +0900

    Feat: 검색 필터 내림 아이콘 추가

--- components/common/SearchFilter.tsx
+++ components/common/SearchFilter.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { DownIcon } from "../Icon";
 import WhiteFilterIcon from "../Icon/WhiteFilterIcon";
 
 export default function SearchFilter() {
@@ -9,11 +10,13 @@ export default function SearchFilter() {
       className="text-white flex flex-col mb-small cursor-pointer"
       onClick={handleClick}
     >
-      <div className="inline-flex items-center">
+      <div className="flex items-center">
         <WhiteFilterIcon />
         <span className="block ml-3 text-middle">필터</span>
+        <DownIcon className="ml-3 mt-1 [&>path]:stroke-white" />
       </div>
       {isOpen && <div>숨김</div>}
+      <hr className="text-white mt-3 mb-10" />
     </div>
   );
 }
