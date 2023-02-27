b8fe9844e1fb55b518b9d7c3c21d284d1f59b486
J1min <aoiharu41@gmail.com>
2023-02-27 14:19:15 +0900

    Feat: add FetchNextPageType

--- types/index.interface.ts
+++ types/index.interface.ts
@@ -1,9 +1,16 @@
 import {
+  FetchNextPageOptions,
+  InfiniteQueryObserverResult,
   QueryObserverResult,
   RefetchOptions,
   RefetchQueryFilters,
 } from "@tanstack/react-query";
+import { PortfolioList } from "./portfolio.interface";
 
 export type RefetchType = <TPageData>(
   options?: RefetchOptions & RefetchQueryFilters<TPageData>,
 ) => Promise<QueryObserverResult>;
+
+export type FetchNextPageType = (
+  options?: FetchNextPageOptions | undefined,
+) => Promise<InfiniteQueryObserverResult<PortfolioList, unknown>>;
