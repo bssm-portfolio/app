4bfc3d5d92c8c1872db9e8f9b7395b970464e132
J1min <aoiharu41@gmail.com>
2023-02-27 12:52:50 +0900

    Fix: 메인페이지 요소 클릭 안되는 버그 수정

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -8,10 +8,12 @@ interface MainLayoutProps {
 
 export default function MainLayout({ app, title, filter }: MainLayoutProps) {
   return (
-    <div className="relative w-full flex flex-col justify-center px-[8.375rem] bg-background_blue overflow-hidden -z-20">
-      <div>{title}</div>
-      <div>{filter}</div>
-      <div>{app}</div>
+    <div className="relative z-10 bg-background_blue">
+      <div className="w-full flex flex-col justify-center px-[8.375rem] overflow-hidden z-20">
+        <div>{title}</div>
+        <div>{filter}</div>
+        <div>{app}</div>
+      </div>
       <div className="absolute top-[37.5rem] -left-16 -rotate-[8deg] bg-somago_yellow w-[110vw] h-60 -z-10" />
     </div>
   );
