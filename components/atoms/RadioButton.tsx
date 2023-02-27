ed4ccd5a056d20a009833ac953951ebd1e5d9eb9
J1min <aoiharu41@gmail.com>
2023-02-27 14:19:44 +0900

    Feat: 라디오 버튼 추가

---
+++ components/atoms/RadioButton.tsx
@@ -0,0 +1,47 @@
+import classNames from "classnames";
+import { Dispatch, SetStateAction } from "react";
+
+interface RadioProps {
+  id: string;
+  label: string;
+  name?: string;
+  checked?: boolean;
+  value?: string;
+  keyword: string;
+  setKeyword: Dispatch<SetStateAction<string>>;
+}
+
+export default function RadioButton({
+  id,
+  label,
+  name,
+  value,
+  keyword,
+  setKeyword,
+  ...props
+}: RadioProps) {
+  return (
+    <div className="flex items-start">
+      <input
+        type="radio"
+        className="appearance-none cursor-pointer"
+        name={name}
+        id={id}
+        value={value}
+        onChange={(event) => setKeyword(event.target.value)}
+        {...props}
+      />
+      <label
+        htmlFor={id}
+        className={classNames(
+          "py-1.5 px-4 bg-white font-bold rounded-full cursor-pointer",
+          {
+            "!bg-somago_yellow": value === keyword,
+          },
+        )}
+      >
+        {label}
+      </label>
+    </div>
+  );
+}
--- components/main/MainFilter.tsx
+++ components/main/MainFilter.tsx
@@ -1,15 +1,70 @@
-import InputButton from "../atoms/InputButton";
+import { useEffect, useState } from "react";
+import RadioButton from "../atoms/RadioButton";
 
 export default function MainFilter() {
+  const [keyword, setKeyword] = useState("");
+  useEffect(() => {
+    console.log(keyword);
+  }, [keyword]);
+
   return (
     <div className="flex items-center justify-center gap-3 my-11">
-      <InputButton>전체</InputButton>
-      <InputButton>추천</InputButton>
-      <InputButton>랭킹순</InputButton>
-      <InputButton>3학년 랭킹순</InputButton>
-      <InputButton>2학년 랭킹순</InputButton>
-      <InputButton>1학년 랭킹순</InputButton>
-      <InputButton>팔로잉</InputButton>
+      <RadioButton
+        id="all"
+        name="main-radio"
+        label="전체"
+        value="전체"
+        keyword={keyword}
+        setKeyword={setKeyword}
+      />
+      <RadioButton
+        id="recommend"
+        name="main-radio"
+        label="추천"
+        value="추천"
+        keyword={keyword}
+        setKeyword={setKeyword}
+      />
+      <RadioButton
+        id="all2"
+        name="main-radio"
+        label="랭킹순"
+        value="랭킹순"
+        keyword={keyword}
+        setKeyword={setKeyword}
+      />
+      <RadioButton
+        id="all3"
+        name="main-radio"
+        label="3학년 랭킹순"
+        value="3학년 랭킹순"
+        keyword={keyword}
+        setKeyword={setKeyword}
+      />
+      <RadioButton
+        id="all4"
+        name="main-radio"
+        label="2학년 랭킹순"
+        value="2학년 랭킹순"
+        keyword={keyword}
+        setKeyword={setKeyword}
+      />
+      <RadioButton
+        id="all5"
+        name="main-radio"
+        label="1학년 랭킹순"
+        value="1학년 랭킹순"
+        keyword={keyword}
+        setKeyword={setKeyword}
+      />
+      <RadioButton
+        id="all6"
+        name="main-radio"
+        label="팔로잉"
+        value="팔로잉"
+        keyword={keyword}
+        setKeyword={setKeyword}
+      />
     </div>
   );
 }
