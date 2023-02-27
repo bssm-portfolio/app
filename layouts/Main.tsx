40135475482a5749c8042b4e69b47d7509a8fbd1
J1min <aoiharu41@gmail.com>
2023-02-27 12:38:25 +0900

    Feat: 메인 노란줄 추가

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -8,10 +8,11 @@ interface MainLayoutProps {
 
 export default function MainLayout({ app, title, filter }: MainLayoutProps) {
   return (
-    <div className="w-full flex flex-col justify-center px-[8.375rem] bg-background_blue">
+    <div className="relative w-full flex flex-col justify-center px-[8.375rem] bg-background_blue overflow-hidden -z-20">
       <div>{title}</div>
       <div>{filter}</div>
       <div>{app}</div>
+      <div className="absolute top-[37.5rem] -left-16 -rotate-[8deg] bg-somago_yellow w-[110vw] h-60 -z-10" />
     </div>
   );
 }
