d9c89e4d2cdf01789ae15d3ee3c473afb9274513
J1min <aoiharu41@gmail.com>
2023-02-28 19:46:47 +0900

    Feat: 유저 정보 수정 폼 추가

--- components/profile/ProfileEdit.tsx
+++ components/profile/ProfileEdit.tsx
@@ -1,3 +1,49 @@
-export default function ProfileEdit() {
-  return <div className="flex gap-20">프로필 수정 페이지입니다</div>;
+import httpClient from "@/apis";
+import useOverlay from "@/hooks/useOverlay";
+import KEY from "@/models/key";
+import { Member } from "@/types/member.interface";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
+import Button from "../atoms/Button";
+import Input from "../atoms/Input";
+
+export default function ProfileEdit({ userInfo }: { userInfo: Member }) {
+  console.log(userInfo);
+  const { register, handleSubmit, reset } = useForm<Member>();
+  const { openToast } = useOverlay();
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    reset(userInfo);
+  }, [reset, userInfo]);
+
+  const onValid: SubmitHandler<Member> = (submitData) => {
+    console.log(submitData);
+    httpClient.member
+      .put({ ...userInfo, ...submitData })
+      .then(() => openToast("수정에 성공하였습니다."))
+      .catch(() => {
+        openToast("수정에 실패하였습니다.");
+        queryClient.invalidateQueries([KEY.MEMBER]);
+      });
+  };
+
+  const onInVaild: SubmitErrorHandler<Member> = (submitData) => {
+    console.log(submitData);
+  };
+
+  return (
+    <form
+      className="flex flex-col gap-4"
+      onSubmit={handleSubmit(onValid, onInVaild)}
+    >
+      <Input registerReturn={register("name")} />
+      <Input registerReturn={register("description")} />
+      <Input registerReturn={register("email")} />
+      <Input registerReturn={register("job")} />
+      <Input registerReturn={register("phone")} />
+      <Button>제출</Button>
+    </form>
+  );
 }
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,6 +1,8 @@
 import ProfileEdit from "@/components/profile/ProfileEdit";
 import ProfileEditLayout from "@/layouts/ProfileEdit";
+import useUser from "@/hooks/useUser";
 
 export default function ProfileEditPage() {
-  return <ProfileEditLayout app={<ProfileEdit />} />;
+  const { user: userInfo } = useUser({ authorizedPage: true });
+  return <ProfileEditLayout app={<ProfileEdit userInfo={userInfo} />} />;
 }
