d682536499accbbff3a286fb0c7f5d1b1fb888d8
J1min <aoiharu41@gmail.com>
2023-02-28 11:33:56 +0900

    Fix: 업로드 버튼 크기 변경

--- components/app/MemberProfile/ProfilePagePortfolioList.tsx
+++ components/app/MemberProfile/ProfilePagePortfolioList.tsx
@@ -35,7 +35,7 @@ export default function ProfilePagePortfolioList({
       })}
       {isMypage && (
         <div
-          className="w-[20rem] h-[11.25rem] flex items-center justify-center rounded-md border border-blue cursor-pointer"
+          className="w-[22.5rem] h-[23.75rem] flex items-center justify-center rounded-md border border-blue cursor-pointer"
           onClick={openUploadModal}
         >
           <div className="flex flex-col items-center">
