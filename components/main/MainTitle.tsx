51ae5f034ac7086069c4112253baeb970e81f298
pkiop <pkiopb@gmail.com>
2023-02-28 20:39:02 +0900

    Feat: 디자인에 맞게 반영

--- components/main/MainTitle.tsx
+++ components/main/MainTitle.tsx
@@ -6,7 +6,7 @@ export default function MainTitle() {
   text-white`;
 
   return (
-    <div>
+    <div className="mt-[2.5rem]">
       <h1 className={h1Style}>BUSAN</h1>
       <h1 className={h1Style}>SOFTWARE MEISTER</h1>
       <h1 className={h1Style}>HIGH SCHOOL</h1>
