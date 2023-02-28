0ce5331bec140cda16ccda9f0e1fbafe3e64c023
J1min <aoiharu41@gmail.com>
2023-02-28 22:30:30 +0900

    Feat: 프로필 수정 기능 추가

--- components/profile/ProfileEdit.tsx
+++ components/profile/ProfileEdit.tsx
@@ -1,7 +1,7 @@
 import httpClient from "@/apis";
 import useOverlay from "@/hooks/useOverlay";
 import KEY from "@/models/key";
-import { Member } from "@/types/member.interface";
+import { Member, MemberType } from "@/types/member.interface";
 import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
@@ -10,27 +10,36 @@ import Input from "../atoms/Input";
 import Select, { Option } from "../common/Select";
 
 export default function ProfileEdit({ userInfo }: { userInfo: Member }) {
-  const { register, handleSubmit, reset, setValue } = useForm<Member>();
+  const { register, handleSubmit, reset, setValue } = useForm<Member>({
+    defaultValues: userInfo,
+  });
   const { openToast } = useOverlay();
   const queryClient = useQueryClient();
+  const isStudent = userInfo.memberType === MemberType.STUDENT;
 
   useEffect(() => {
     reset(userInfo);
   }, [reset, userInfo]);
 
   const onValid: SubmitHandler<Member> = (validData) => {
+    const payload = {
+      ...userInfo,
+      ...validData,
+      schoolGrade: isStudent ? validData.schoolGrade : undefined,
+      schoolClass: isStudent ? validData.schoolClass : undefined,
+      schoolNumber: isStudent ? validData.schoolNumber : undefined,
+    };
     httpClient.member
-      .put({ ...userInfo, ...validData })
+      .put(payload)
       .then(() => openToast("수정에 성공하였습니다."))
       .catch(() => {
-        openToast("수정에 실패하였습니다.");
+        openToast("수정에 실패하였습니다.", { type: "danger" });
         queryClient.invalidateQueries([KEY.MEMBER]);
       });
   };
 
-  const onInVaild: SubmitErrorHandler<Member> = (inValidData) => {
-    console.log(inValidData);
-  };
+  const onInVaild: SubmitErrorHandler<Member> = () =>
+    openToast("잘못된 정보입니다.", { type: "danger" });
 
   return (
     <form
@@ -42,39 +51,43 @@ export default function ProfileEdit({ userInfo }: { userInfo: Member }) {
       <Input registerReturn={register("email")} />
       <Input registerReturn={register("job")} />
       <Input registerReturn={register("phone")} />
-      <div className="flex justify-between gap-1">
-        <Select
-          className="w-full"
-          placeholder="학년"
-          setValue={(v: Option) =>
-            typeof v.value === "number" && setValue("schoolGrade", v.value)
-          }
-          options={Array(3)
-            .fill(null)
-            .map((_, i) => ({ label: String(i + 1), value: i + 1 }))}
-        />
-        <Select
-          className="w-full"
-          placeholder="반"
-          setValue={(v: Option) =>
-            typeof v.value === "number" && setValue("schoolClass", v.value)
-          }
-          options={Array(4)
-            .fill(null)
-            .map((_, i) => ({ label: String(i + 1), value: i + 1 }))}
-        />
-        <Select
-          className="w-full"
-          placeholder="번호"
-          setValue={(v: Option) =>
-            typeof v.value === "number" && setValue("schoolNumber", v.value)
-          }
-          options={Array(20)
-            .fill(null)
-            .map((_, i) => ({ label: String(i + 1), value: i + 1 }))}
-        />
-        <Input registerReturn={register("admissionYear")} />
-      </div>
+      {isStudent && (
+        <div className="flex justify-between gap-1">
+          <Select
+            className="w-full"
+            placeholder="학년"
+            setValue={(v: Option) =>
+              typeof v.value === "number" && setValue("schoolGrade", v.value)
+            }
+            options={Array(3)
+              .fill(null)
+              .map((_, i) => ({ label: String(i + 1), value: i + 1 }))}
+            defaultValue={userInfo.schoolGrade.toString()}
+          />
+          <Select
+            className="w-full"
+            placeholder="반"
+            setValue={(v: Option) =>
+              typeof v.value === "number" && setValue("schoolClass", v.value)
+            }
+            options={Array(4)
+              .fill(null)
+              .map((_, i) => ({ label: String(i + 1), value: i + 1 }))}
+            defaultValue={userInfo.schoolClass.toString()}
+          />
+          <Select
+            className="w-full"
+            placeholder="번호"
+            setValue={(v: Option) =>
+              typeof v.value === "number" && setValue("schoolNumber", v.value)
+            }
+            options={Array(20)
+              .fill(null)
+              .map((_, i) => ({ label: String(i + 1), value: i + 1 }))}
+            defaultValue={userInfo.schoolNumber.toString()}
+          />
+        </div>
+      )}
       <Button type="submit">제출</Button>
     </form>
   );
