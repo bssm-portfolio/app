e18964341c4af40e0935b99f48592e3d720d507e
pkiop <pkiopb@gmail.com>
2023-02-28 21:20:33 +0900

    Fix: app 세로 가운데정렬 제거

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -9,7 +9,7 @@ interface MainLayoutProps {
 export default function MainLayout({ app, title, filter }: MainLayoutProps) {
   return (
     <section className="relative z-10 bg-background_blue overflow-hidden flex justify-center min-h-[calc(100vh-5.125rem)]">
-      <div className="flex flex-col justify-center z-20 max-w-[73.75rem] w-[90vw]">
+      <div className="flex flex-col z-20 max-w-[73.75rem] w-[90vw]">
         <div>{title}</div>
         <div>{filter}</div>
         <div>{app}</div>
