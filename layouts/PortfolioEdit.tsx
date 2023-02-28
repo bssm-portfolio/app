1c928005fe165ef4dd71d8b2d1e92725a66a4f2e
J1min <aoiharu41@gmail.com>
2023-02-28 11:58:26 +0900

    Feat: min-height :100vh 추가

--- layouts/PortfolioEdit.tsx
+++ layouts/PortfolioEdit.tsx
@@ -5,7 +5,7 @@ interface FrameProps {
 }
 
 function Frame({ app }: FrameProps) {
-  return <section>{app}</section>;
+  return <section className="min-h-[calc(100vh-5.125rem)]">{app}</section>;
 }
 
 export default function EditLayout({ app }: FrameProps) {
--- layouts/Search.tsx
+++ layouts/Search.tsx
@@ -7,7 +7,7 @@ interface SearchPageLayoutProps {
 
 function Frame({ filter, portfolioList }: SearchPageLayoutProps) {
   return (
-    <div className="relative pt-8 px-32 flex flex-col justify-center z-10 overflow-hidden">
+    <div className="relative pt-8 px-32 flex flex-col z-10 overflow-hidden min-h-[calc(100vh-5.125rem)]">
       <div className="z-20">
         <div>{filter}</div>
         <div>{portfolioList}</div>
