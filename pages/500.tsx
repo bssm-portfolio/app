f1eed31ea3a87adb08a3de71e1a0ba7f19347123
J1min <aoiharu41@gmail.com>
2023-03-01 13:07:36 +0900

    Feat: 500 페이지 디자인 변경

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,7 +1,12 @@
+import Button from "@/components/atoms/Button";
+
 export default function Page500() {
   return (
-    <div className="bg-background_blue min-h-[calc(100vh-4.625rem)]">
-      <h1 className="text-5xl text-white font-bold pt-44 pl-36">500</h1>
+    <div className="min-h-[calc(100vh-4.625rem)] pl-36">
+      <h1 className="text-[4rem] text-blue font-bold pt-44 ">500 Error</h1>
+      <Button className="text-3xl mt-24 py-3 px-28 rounded-full font-semibold">
+        홈으로 가기
+      </Button>
     </div>
   );
 }
