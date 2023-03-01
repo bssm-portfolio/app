b1461b5102cea38257b9da914c09f9616f7e4781
J1min <aoiharu41@gmail.com>
2023-03-01 15:04:04 +0900

    Feat: add markdown view

--- components/portfolio/Description.tsx
+++ components/portfolio/Description.tsx
@@ -1,10 +1,10 @@
 import Markdown from "marked-react";
 import { Description } from "@/types/portfolio.interface";
 
-export default function DescriptionView({ description }: Description) {
+export default function DescriptionView({ children }: Description) {
   return (
-    <div className="px-large py-small bg-primary-light_gray rounded">
-      <Markdown>{description}</Markdown>
+    <div className="prose">
+      <Markdown>{children}</Markdown>
     </div>
   );
 }
