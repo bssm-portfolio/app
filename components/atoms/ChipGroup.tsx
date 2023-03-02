6bbbaf3859c846319e613b92c3778aa55a72393b
J1min <aoiharu41@gmail.com>
2023-03-02 14:25:06 +0900

    Feat: ChipGroup 추가

---
+++ components/atoms/ChipGroup.tsx
@@ -0,0 +1,31 @@
+import { PortfolioListType } from "@/types/portfolio.interface";
+import { Skill } from "@/types/skill.interface";
+import Chip from "./Chip";
+
+interface ChipGroupProps {
+  skillList: Skill[];
+  type?: PortfolioListType;
+}
+
+export default function ChipGroup({
+  skillList,
+  type = "main",
+}: ChipGroupProps) {
+  const getFilteredSkillList = () => {
+    if (type !== "detail") return skillList.filter((_, idx) => idx < 3);
+    return skillList;
+  };
+
+  return (
+    <Chip.Group className="pt-2" type={type}>
+      {getFilteredSkillList().map((skillData) => (
+        <Chip.Item type={type} key={skillData.skillId}>
+          {skillData.skillName}
+        </Chip.Item>
+      ))}
+      {type !== "detail" && skillList.length > 2 && (
+        <Chip.Item>더보기</Chip.Item>
+      )}
+    </Chip.Group>
+  );
+}
