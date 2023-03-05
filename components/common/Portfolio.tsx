32724885192923079b91afdf0c053808b4937fa7
J1min <aoiharu41@gmail.com>
2023-03-05 21:30:18 +0900

    Fix: 포트폴리오 레이아웃 깨짐 수정

--- components/common/Portfolio.tsx
+++ components/common/Portfolio.tsx
@@ -39,44 +39,43 @@ export default function PortfolioView({ portfolio, onClick }: PortfolioProps) {
           onError={() => setImageSrc(config.defaultThumbnail)}
         />
       </div>
-      <div className="flex mt-3 mb-[3.125rem] ">
-        <div className="flex flex-col">
-          <div className="flex">
-            <div className="flex gap-4">
-              <Avatar
-                width={40}
-                height={40}
-                imageUrl={portfolio.writer.profileImageUrl}
-              />
-              <div className="flex-1">
-                <div className="font-bold text-sm line-clamp-2 break-all">
-                  {portfolio.title}
-                </div>
-                <div className="text-xs">
-                  {portfolio.writer.name}
-                  {portfolio.contributorList.length > 0
-                    ? `외 ${portfolio.contributorList.length} 명`
-                    : ""}
-                </div>
-                <ChipGroup skillList={portfolio.skillList} />
+      <div className="mt-3 mb-[3.125rem]">
+        <div className="flex justify-between">
+          <div className="flex gap-4">
+            <Avatar
+              width={40}
+              height={40}
+              imageUrl={portfolio.writer.profileImageUrl}
+            />
+            <div className="flex-1">
+              <div className="font-bold text-sm line-clamp-2 break-all">
+                {portfolio.title}
               </div>
+              <div className="text-xs">
+                {portfolio.writer.name}
+                {portfolio.contributorList.length > 0
+                  ? `외 ${portfolio.contributorList.length} 명`
+                  : ""}
+              </div>
+              <ChipGroup skillList={portfolio.skillList} />
             </div>
-            <div className="flex gap-3 items-start">
-              <div className={countViewCss}>
-                <HeartIcon
-                  className={classNames(
-                    portfolio.bookmarkYn
-                      ? "[&_path]:!fill-somago_yellow"
-                      : undefined,
-                    "mr-[0.1rem]",
-                  )}
-                />
-                {portfolio.bookmarks}
-              </div>
-              <div className={countViewCss}>
-                <CommentIcon className={classNames("mr-[0.1rem]")} />
-                {portfolio.comments}
-              </div>
+          </div>
+
+          <div className="flex gap-3 items-start">
+            <div className={countViewCss}>
+              <HeartIcon
+                className={classNames(
+                  portfolio.bookmarkYn
+                    ? "[&_path]:!fill-somago_yellow"
+                    : undefined,
+                  "mr-[0.1rem]",
+                )}
+              />
+              {portfolio.bookmarks}
+            </div>
+            <div className={countViewCss}>
+              <CommentIcon className={classNames("mr-[0.1rem]")} />
+              {portfolio.comments}
             </div>
           </div>
         </div>
