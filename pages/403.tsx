a8975a04414240d68142c27d106bcc5149b68c60
J1min <aoiharu41@gmail.com>
2023-03-18 13:39:04 +0900

    Feat: 403 페이지 추가, 버튼 padding 수정

--- pages/403.tsx
+++ pages/403.tsx
@@ -1,16 +1,13 @@
 import Button from "@/components/atoms/Button";
 import { useRouter } from "next/router";
 
-export default function Page404() {
+export default function Page500() {
   const router = useRouter();
   return (
     <div className="min-h-[calc(100vh-4.625rem)] flex flex-col items-center">
-      <h1 className="text-[4rem] text-blue font-bold pt-44">404 Error</h1>
-      <h1 className="text-[3.25rem] mt-3 text-blue font-bold">
-        PAGE NOT FOUND
+      <h1 className="text-[3rem] text-blue font-bold pt-44">
+        비공개 포트폴리오입니다.
       </h1>
-      <p className="pt-14">죄송합니다. 페이지를 찾을 수 없습니다.</p>
-      <p>이 페이지는 삭제되었거나 URL이 변경되었을 수 있습니다.</p>
       <Button
         className="mt-24 !py-3 !px-32 rounded-full font-semibold"
         onClick={() => router.push("/")}
--- pages/500.tsx
+++ pages/500.tsx
@@ -8,7 +8,7 @@ export default function Page500() {
       <h1 className="text-[4rem] text-blue font-bold pt-44">500 Error</h1>
       <p className="pt-14">죄송합니다. 페이지를 찾을 수 없습니다.</p>
       <Button
-        className="mt-24 py-3 px-32 rounded-full font-semibold"
+        className="mt-24 !py-3 !px-32 rounded-full font-semibold"
         onClick={() => router.push("/")}
       >
         <span className="text-base">홈으로 이동</span>
