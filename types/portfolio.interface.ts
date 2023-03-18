c0dfd380ad59f547955519d4c0495486bd0faa85
J1min <aoiharu41@gmail.com>
2023-03-19 01:56:19 +0900

    Fix: 포트폴리오 검색SchoolGradeType 0 제거

--- types/portfolio.interface.ts
+++ types/portfolio.interface.ts
@@ -82,7 +82,7 @@ export type SortType =
   | "VIEWS"
   | "ALL";
 export type SortDirectionType = "ASC" | "DESC";
-export type SchoolGradeType = 0 | 1 | 2 | 3;
+export type SchoolGradeType = 1 | 2 | 3;
 export type PortfolioThemeType = "WEB" | "APP" | "EMBEDDED" | "ROBOT";
 export type SearchType =
   | "TITLE"
