b2f3bd7733af8b2d490dead1afba51d9d4e890d4
J1min <aoiharu41@gmail.com>
2023-03-25 21:37:53 +0900

    Feat: Video 컴포넌트 추가

--- components/atoms/Video.tsx
+++ components/atoms/Video.tsx
@@ -2,8 +2,13 @@ import useWindow from "@/hooks/useWindow";
 import { ReactPlayerProps } from "react-player";
 import ReactPlayer from "react-player/lazy";
 
-export default function Video({ ...props }: ReactPlayerProps) {
+interface VideoProps extends ReactPlayerProps {
+  src?: string | string[];
+}
+
+export default function Video({ src, ...props }: VideoProps) {
   const { isWindow } = useWindow();
   if (!isWindow) return <div />;
-  return <ReactPlayer {...props} />;
+
+  return <ReactPlayer width="100%" url={src} {...props} />;
 }
