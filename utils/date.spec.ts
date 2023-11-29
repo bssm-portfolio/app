3a362f4bf79df9ede6ded6e8b52d1660140a53c3
J1min <aoiharu41@gmail.com>
2023-11-29 09:33:27 +0900

    fix(test): 하드코딩으로 진행한 테스트 제거

--- utils/date.spec.ts
+++ utils/date.spec.ts
@@ -8,14 +8,6 @@ test("koreanDate test - after 2000", () => {
   expect(getKoreanDate(new Date("December 17, 2005"))).toBe("2005년 12월 17일");
 });
 
-test("timeAgo test - before 2000", () => {
-  expect(getTimeAgo(new Date("December 17, 1995"))).toBe("27년 전");
-});
-
-test("timeAgo test - after 2000", () => {
-  expect(getTimeAgo(new Date("December 17, 2005"))).toBe("17년 전");
-});
-
 test("timeAgo test - now", () => {
   expect(getTimeAgo(new Date())).toBe("방금");
 });
